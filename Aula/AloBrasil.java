a60a7f730c0f4546644511ba4b03d2a06a00fc69
Markinn07 <marcospaulogomesdealmeida746@gmail.com>
2025-10-09 23:57:43 +0000

    Pri,eira atividade

---
+++ Aula/AloBrasil.java
@@ -0,0 +1,8 @@
+// Autor: Marcos Paulo Gomes de Almeida 
+// Data: 02/10/2025 
+
+public class AloBrasil { 
+    public static void main(String[] args) {
+        System.out.println("Alô, Brasil!");
+    }
+}
